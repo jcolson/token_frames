2a0cc8eb20963f08a43a30b667e67b4596737837
Jay Colson <jay@karma.net>
2022-07-01 15:46:58 +0100

    updated make scipt to list the things that should be added to the release

--- make.sh
+++ make.sh
@@ -25,4 +25,8 @@ cd ..
 cd Sourced_From_EldritchImp
 zip -ur ../TokenFrames.zip token_frames
 cd ..
-printf "\033[0;31mRemember that in order to make a release, you need to tag this version with the module.json version and upload the new TokenFrames.zip as WELL as the module.json into the github release!\033[0m\n"
+ver=`grep \"version\" module.json`
+printf "\033[0;34mRemember that in order to make a release:\033[0m\n"
+printf "\033[0;31m - git tag this version with the module.json version (${ver})\n"
+printf " - upload the new TokenFrames.zip as WELL as the module.json into the github release!\n"
+printf " - update the CHANGELOG.md file with this version.\033[0m\n"
